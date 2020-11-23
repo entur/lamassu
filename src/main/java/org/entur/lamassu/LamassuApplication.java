81943736814175c559e992d1aa6b9bf927261c52
Tom Erik Støwer <testower@gmail.com>
2020-11-23 15:46:59 +0100

    Initial commit

---
+++ src/main/java/org/entur/lamassu/LamassuApplication.java
@@ -0,0 +1,13 @@
+package org.entur.lamassu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LamassuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LamassuApplication.class, args);
+    }
+
+}
