a19a4d09a02e0b0d5ca0b75d39e83eaa4f3d0acc
Tom Erik Støwer <testower@gmail.com>
2020-11-24 21:47:43 +0100

    Imrpove configuration of redis

--- src/main/java/org/entur/lamassu/config/RedissonConfig.java
+++ src/main/java/org/entur/lamassu/config/RedissonConfig.java
@@ -2,9 +2,10 @@ package org.entur.lamassu.config;
 
 import org.redisson.config.Config;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class RedissonConfig {
     private final Config config;
 
--- src/main/java/org/entur/lamassu/config/RedissonSpringDataConfig.java
+++ src/main/java/org/entur/lamassu/config/RedissonSpringDataConfig.java
@@ -3,7 +3,6 @@ package org.entur.lamassu.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,7 +15,7 @@ public class RedissonSpringDataConfig {
     }
 
     @Bean(destroyMethod = "shutdown")
-    public RedissonClient redisson(@Autowired RedissonConfig redissonConfig) {
+    public RedissonClient redisson(RedissonConfig redissonConfig) {
         return Redisson.create(redissonConfig.getConfig());
     }
 }
