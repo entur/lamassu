8a604fbcd170245a3dc954935ed313ae85ce554c
Tom Erik Støwer <testower@gmail.com>
2020-11-24 20:15:42 +0100

    Use embedded redis for tests

--- src/test/java/org/entur/lamassu/LamassuApplicationTests.java
+++ src/test/java/org/entur/lamassu/LamassuApplicationTests.java
@@ -1,9 +1,10 @@
 package org.entur.lamassu;
 
+import org.entur.lamassu.config.TestRedisConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TestRedisConfiguration.class)
 class LamassuApplicationTests {
 
     @Test
---
+++ src/test/java/org/entur/lamassu/config/TestRedisConfiguration.java
@@ -0,0 +1,28 @@
+package org.entur.lamassu.config;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class TestRedisConfiguration {
+
+    private RedisServer redisServer;
+
+    public TestRedisConfiguration(RedisProperties redisProperties) {
+        this.redisServer = new RedisServer(redisProperties.getPort());
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+}
