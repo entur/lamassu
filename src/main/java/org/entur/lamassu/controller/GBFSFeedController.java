00747ed5e597869c87f7f776a2261629f061eda4
Tom Erik Støwer <testower@gmail.com>
2020-12-01 11:58:21 +0100

    Handle unknown gbfs feed name enum value as not found

--- src/main/java/org/entur/lamassu/controller/GBFSFeedController.java
+++ src/main/java/org/entur/lamassu/controller/GBFSFeedController.java
@@ -4,9 +4,11 @@ import org.entur.lamassu.cache.GBFSFeedCache;
 import org.entur.lamassu.model.gbfs.v2_1.GBFSBase;
 import org.entur.lamassu.model.gbfs.v2_1.GBFSFeedName;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class GBFSFeedController {
@@ -26,7 +28,11 @@ public class GBFSFeedController {
 
     @GetMapping("/gbfs/{codespace}/{city}/{vehicleType}/{feed}")
     public GBFSBase getGbfsFeedForProvider(@PathVariable String codespace, @PathVariable String city, @PathVariable String vehicleType, @PathVariable String feed) {
-        GBFSFeedName feedName = GBFSFeedName.valueOf(feed.toUpperCase());
-        return feedCache.find(feedName, codespace, city, vehicleType);
+        try {
+            GBFSFeedName feedName = GBFSFeedName.valueOf(feed.toUpperCase());
+            return feedCache.find(feedName, codespace, city, vehicleType);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
