8b34348bbede8b902582db345a582748b97446ff
Tom Erik Støwer <testower@gmail.com>
2021-02-02 10:27:03 +0100

    WTK-321 Add queries for codespaces and operators

--- src/main/java/org/entur/lamassu/controller/GraphQLQueryController.java
+++ src/main/java/org/entur/lamassu/controller/GraphQLQueryController.java
@@ -1,6 +1,9 @@
 package org.entur.lamassu.controller;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import org.entur.lamassu.config.feedprovider.FeedProviderConfig;
+import org.entur.lamassu.model.FeedProvider;
+import org.entur.lamassu.model.Operator;
 import org.entur.lamassu.model.Vehicle;
 import org.entur.lamassu.service.VehiclesNearbyService;
 import org.slf4j.Logger;
@@ -9,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Component
 public class GraphQLQueryController implements GraphQLQueryResolver {
@@ -16,6 +20,9 @@ public class GraphQLQueryController implements GraphQLQueryResolver {
 
     @Autowired
     VehiclesNearbyService vehiclesNearbyService;
+
+    @Autowired
+    FeedProviderConfig feedProviderConfig;
 
     public Collection<Vehicle> vehicles(
             Double lat,
@@ -30,5 +37,20 @@ public class GraphQLQueryController implements GraphQLQueryResolver {
                 range,
                 count
         );
+    }
+
+    public Collection<String> codespaces() {
+        return feedProviderConfig.getProviders().stream().map(FeedProvider::getCodespace).collect(Collectors.toSet());
+    }
+
+    public Collection<Operator> operators() {
+        return feedProviderConfig.getProviders().stream().map(this::mapToOperator).collect(Collectors.toList());
+    }
+
+    private Operator mapToOperator(FeedProvider feedProvider) {
+        var operator = new Operator();
+        operator.setName(feedProvider.getName());
+        operator.setCodespace(feedProvider.getCodespace());
+        return operator;
     }
 }
---
+++ src/main/java/org/entur/lamassu/model/Operator.java
@@ -0,0 +1,22 @@
+package org.entur.lamassu.model;
+
+public class Operator {
+    private String name;
+    private String codespace;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodespace() {
+        return codespace;
+    }
+
+    public void setCodespace(String codespace) {
+        this.codespace = codespace;
+    }
+}
