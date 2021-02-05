e6039a38143fd4c063eb7bee9bf87b7c9366c4d1
Tom Erik Støwer <testower@gmail.com>
2021-02-05 13:41:56 +0100

    WTK-320 Revert autowiring change for quartz job

--- src/main/java/org/entur/lamassu/updater/FetchDiscoveryFeedsJob.java
+++ src/main/java/org/entur/lamassu/updater/FetchDiscoveryFeedsJob.java
@@ -7,15 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Component;
 
-@Component
 public class FetchDiscoveryFeedsJob extends QuartzJobBean {
 
-    private final FeedUpdateService feedUpdateService;
-
+    // This works thanks to org.entur.lamassu.util.AutowiringSpringBeanJobFactory
     @Autowired
-    public FetchDiscoveryFeedsJob(FeedUpdateService feedUpdateService) {
-        this.feedUpdateService = feedUpdateService;
-    }
+    private FeedUpdateService feedUpdateService;
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
