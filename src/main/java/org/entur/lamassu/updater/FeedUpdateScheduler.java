b7300befc768c409f74b472c150bb527d16fe554
Tom Erik Støwer <testower@gmail.com>
2021-09-24 20:49:48 +0200

    Remove unused imports

--- src/main/java/org/entur/lamassu/updater/FeedUpdateScheduler.java
+++ src/main/java/org/entur/lamassu/updater/FeedUpdateScheduler.java
@@ -1,23 +1,15 @@
 package org.entur.lamassu.updater;
 
-import org.entur.gbfs.GbfsDelivery;
 import org.entur.gbfs.GbfsSubscriptionManager;
-import org.entur.gbfs.GbfsSubscriptionOptions;
-import org.entur.gbfs.v2_2.gbfs.GBFSFeedName;
 import org.entur.lamassu.cache.GBFSFeedCacheV2;
 import org.entur.lamassu.config.feedprovider.FeedProviderConfig;
 import org.entur.lamassu.mapper.DiscoveryFeedMapper;
-import org.entur.lamassu.model.provider.FeedProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 
 @Component
