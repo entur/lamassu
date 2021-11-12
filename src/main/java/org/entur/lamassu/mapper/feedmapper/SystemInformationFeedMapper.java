f9306f50a6805d1f8d58382ed1a92ec5922b2973
Tom Erik Støwer <testower@gmail.com>
2021-11-12 13:58:18 +0100

    WTK-1281 Map system information to override language and optionally operator name

--- src/main/java/org/entur/lamassu/mapper/feedmapper/SystemInformationFeedMapper.java
+++ src/main/java/org/entur/lamassu/mapper/feedmapper/SystemInformationFeedMapper.java
@@ -19,6 +19,7 @@
 package org.entur.lamassu.mapper.feedmapper;
 
 import org.entur.gbfs.v2_2.gbfs.GBFSGbfs;
+import org.entur.gbfs.v2_2.system_information.GBFSData;
 import org.entur.gbfs.v2_2.system_information.GBFSSystemInformation;
 import org.entur.lamassu.model.provider.FeedProvider;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,13 +30,35 @@ public class SystemInformationFeedMapper implements FeedMapper<GBFSSystemInforma
     @Value("${org.entur.lamassu.targetGbfsVersion:2.2}")
     private String targetGbfsVersion;
 
+    @Value("${org.entur.lamassu.targetLanguageCode:nb}")
+    private String targetLanguageCode;
+
     @Override
     public GBFSSystemInformation map(GBFSSystemInformation source, FeedProvider feedProvider) {
         var mapped = new GBFSSystemInformation();
         mapped.setVersion(GBFSSystemInformation.Version.fromValue(targetGbfsVersion));
         mapped.setLastUpdated(source.getLastUpdated());
         mapped.setTtl(source.getTtl());
-        mapped.setData(source.getData());
+        mapped.setData(mapData(source.getData(), feedProvider));
+        return mapped;
+    }
+
+    private GBFSData mapData(GBFSData source, FeedProvider feedProvider) {
+        var mapped = new GBFSData();
+        mapped.setSystemId(source.getSystemId());
+        mapped.setLanguage(targetLanguageCode);
+        mapped.setName(source.getName());
+        mapped.setShortName(source.getShortName());
+        mapped.setOperator(source.getOperator() != null ? source.getOperator() : feedProvider.getOperatorName());
+        mapped.setUrl(source.getUrl());
+        mapped.setPurchaseUrl(source.getPurchaseUrl());
+        mapped.setStartDate(source.getStartDate());
+        mapped.setPhoneNumber(source.getPhoneNumber());
+        mapped.setEmail(source.getEmail());
+        mapped.setFeedContactEmail(source.getFeedContactEmail());
+        mapped.setTimezone(source.getTimezone());
+        mapped.setLicenseUrl(source.getLicenseUrl());
+        mapped.setRentalApps(source.getRentalApps());
         return mapped;
     }
 }
