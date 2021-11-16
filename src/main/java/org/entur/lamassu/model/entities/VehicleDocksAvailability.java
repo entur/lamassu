43ef8a28bd5a04f309a8ed75f87c1a47f1ba4018
Tom Erik Støwer <testower@gmail.com>
2021-11-16 20:31:48 +0100

    Add support for vehicle_docks_availability in graphql api

---
+++ src/main/java/org/entur/lamassu/model/entities/VehicleDocksAvailability.java
@@ -0,0 +1,51 @@
+/*
+ *
+ *
+ *  * Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+ *  * the European Commission - subsequent versions of the EUPL (the "Licence");
+ *  * You may not use this work except in compliance with the Licence.
+ *  * You may obtain a copy of the Licence at:
+ *  *
+ *  *   https://joinup.ec.europa.eu/software/page/eupl
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the Licence is distributed on an "AS IS" basis,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the Licence for the specific language governing permissions and
+ *  * limitations under the Licence.
+ *
+ */
+
+package org.entur.lamassu.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class VehicleDocksAvailability implements Serializable {
+    private List<VehicleType> vehicleTypes;
+    private Integer count;
+
+    public List<VehicleType> getVehicleTypes() {
+        return vehicleTypes;
+    }
+
+    public void setVehicleTypes(List<VehicleType> vehicleTypes) {
+        this.vehicleTypes = vehicleTypes;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return "VehicleDocksAvailability{" +
+                "vehicleTypes=" + vehicleTypes +
+                ", count=" + count +
+                '}';
+    }
+}
