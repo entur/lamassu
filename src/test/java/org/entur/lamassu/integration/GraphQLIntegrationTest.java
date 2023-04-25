e15751fb69cea8738d85eb1592502e06a94a7db4
Tom Erik Støwer <testower@gmail.com>
2023-04-25 14:23:15 +0200

    Test coverage

--- src/test/java/org/entur/lamassu/integration/GraphQLIntegrationTest.java
+++ src/test/java/org/entur/lamassu/integration/GraphQLIntegrationTest.java
@@ -21,15 +21,30 @@ public class GraphQLIntegrationTest extends AbstractIntegrationTestBase {
     public void testVehiclesQuery() throws IOException {
         GraphQLResponse response = graphQLTestTemplate.postForResource("vehicles_query_with_disabled.graphql");
         assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(2, response.get("$.data.vehicles", List.class).size());
         assertEquals("TST:Vehicle:1234", response.get("$.data.vehicles[0].id"));
         assertEquals("Test", response.get("$.data.vehicles[0].system.name.translation[0].value"));
+    }
+
+    @Test
+    public void testVehicleByIdQuery() throws IOException {
+        GraphQLResponse response = graphQLTestTemplate.postForResource("vehicle_by_id_query.graphql");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("TST:Vehicle:1235", response.get("$.data.vehicleById.id"));
+    }
+
+    @Test
+    public void testVehiclesByIdQuery() throws IOException {
+        GraphQLResponse response = graphQLTestTemplate.postForResource("vehicles_by_id_query.graphql");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("TST:Vehicle:1235", response.get("$.data.vehiclesById[0].id"));
     }
 
     @Test
     public void testVehicleQueryWithoutDisabled() throws IOException {
         GraphQLResponse response = graphQLTestTemplate.postForResource("vehicles_query_without_disabled.graphql");
         assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertTrue(response.get("$.data.vehicles", List.class).isEmpty());
+        assertEquals(1, response.get("$.data.vehicles", List.class).size());
     }
 
     @Test
@@ -39,6 +54,14 @@ public class GraphQLIntegrationTest extends AbstractIntegrationTestBase {
         assertEquals("TST:Station:2", response.get("$.data.stations[0].id"));
         assertEquals("Cooler bikes", response.get("$.data.stations[0].name.translation[0].value"));
         assertEquals("https://rentmybikes.com", response.get("$.data.stations[0].rentalUris.web"));
+    }
+
+    @Test
+    public void testStationByIdQuery() throws IOException {
+        GraphQLResponse response = graphQLTestTemplate.postForResource("station_by_id_query.graphql");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("TST:Station:1", response.get("$.data.stationById.id"));
+        assertEquals("2", response.get("$.data.stationById.vehicleDocksAvailable[0].count"));
     }
 
     @Test
