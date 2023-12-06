f588b32273e595472f8044d4b815cd0ddfcf829d
Tom Erik Støwer <testower@gmail.com>
2023-12-06 09:38:25 +0100

    Formatting

--- src/main/java/org/entur/lamassu/LamassuApplication.java
+++ src/main/java/org/entur/lamassu/LamassuApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class LamassuApplication {
 
   public static void main(String[] args) {
