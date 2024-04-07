adfe2c47c5c1afeb1748756edfc5a4542a4fc683
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-04-07 10:55:46 +0200

    Remove unused import

--- src/main/java/org/entur/lamassu/config/cache/RedissonCacheConfig.java
+++ src/main/java/org/entur/lamassu/config/cache/RedissonCacheConfig.java
@@ -17,7 +17,6 @@ import org.redisson.api.RedissonClient;
 import org.redisson.codec.Kryo5Codec;
 import org.redisson.config.Config;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
