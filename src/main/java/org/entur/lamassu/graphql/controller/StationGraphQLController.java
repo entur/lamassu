f42f360c00aef85c5d794a735afba719d6c17b30
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-01-03 11:53:39 +0100

    Lookup pricing plans for all possible vehicle types for a station

--- src/main/java/org/entur/lamassu/graphql/controller/StationGraphQLController.java
+++ src/main/java/org/entur/lamassu/graphql/controller/StationGraphQLController.java
@@ -3,7 +3,10 @@ package org.entur.lamassu.graphql.controller;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.entur.lamassu.cache.PricingPlanCache;
 import org.entur.lamassu.cache.StationCache;
 import org.entur.lamassu.cache.SystemCache;
@@ -14,8 +17,11 @@ import org.entur.lamassu.model.entities.PropulsionType;
 import org.entur.lamassu.model.entities.Station;
 import org.entur.lamassu.model.entities.System;
 import org.entur.lamassu.model.entities.VehicleDocksAvailability;
+import org.entur.lamassu.model.entities.VehicleDocksCapacity;
 import org.entur.lamassu.model.entities.VehicleType;
 import org.entur.lamassu.model.entities.VehicleTypeAvailability;
+import org.entur.lamassu.model.entities.VehicleTypeCapacity;
+import org.entur.lamassu.model.entities.VehicleTypesCapacity;
 import org.entur.lamassu.service.*;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -146,23 +152,57 @@ public class StationGraphQLController extends BaseGraphQLController {
    */
   @SchemaMapping(typeName = "Station", field = "pricingPlans")
   public List<PricingPlan> getPricingPlans(Station station) {
+    Set<String> vehicleTypeIds = Stream
+      .of(
+        Optional
+          .ofNullable(station.getVehicleCapacity())
+          .orElse(List.of())
+          .stream()
+          .map(VehicleTypeCapacity::getVehicleTypeId),
+        Optional
+          .ofNullable(station.getVehicleDocksCapacity())
+          .orElse(List.of())
+          .stream()
+          .map(VehicleDocksCapacity::getVehicleTypeIds)
+          .flatMap(Collection::stream),
+        Optional
+          .ofNullable(station.getVehicleTypeCapacity())
+          .orElse(List.of())
+          .stream()
+          .map(VehicleTypeCapacity::getVehicleTypeId),
+        Optional
+          .ofNullable(station.getVehicleTypesCapacity())
+          .orElse(List.of())
+          .stream()
+          .map(VehicleTypesCapacity::getVehicleTypeIds)
+          .flatMap(Collection::stream),
+        Optional
+          .ofNullable(station.getVehicleTypesAvailable())
+          .orElse(List.of())
+          .stream()
+          .map(VehicleTypeAvailability::getVehicleTypeId),
+        Optional
+          .ofNullable(station.getVehicleDocksAvailable())
+          .orElse(List.of())
+          .stream()
+          .map(VehicleDocksAvailability::getVehicleTypeIds)
+          .flatMap(Collection::stream)
+      )
+      .flatMap(i -> i)
+      .collect(Collectors.toSet());
+
+    List<VehicleType> vehicleTypes = vehicleTypeCache.getAll(vehicleTypeIds);
+
     Set<String> pricingPlanIds = new HashSet<>();
 
-    // example:
-    station
-      .getVehicleTypesAvailable()
-      .stream()
-      .map(this::getVehicleType)
-      .forEach(vehicleType -> {
-        if (vehicleType.getPricingPlanIds() != null) {
-          pricingPlanIds.addAll(vehicleType.getPricingPlanIds());
-        }
-        if (vehicleType.getDefaultPricingPlanId() != null) {
-          pricingPlanIds.add(vehicleType.getDefaultPricingPlanId());
-        }
-      });
-
-    // should add from other vehicle types as well
+    vehicleTypes.forEach(vehicleType -> {
+      if (vehicleType.getPricingPlanIds() != null) {
+        pricingPlanIds.addAll(vehicleType.getPricingPlanIds());
+      }
+      if (vehicleType.getDefaultPricingPlanId() != null) {
+        pricingPlanIds.add(vehicleType.getDefaultPricingPlanId());
+      }
+    });
 
     return pricingPlanCache.getAll(pricingPlanIds);
   }
