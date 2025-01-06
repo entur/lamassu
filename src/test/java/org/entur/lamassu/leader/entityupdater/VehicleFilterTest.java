10a2dc3c98b8f5b2c9c78e42162d0f157fc03656
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-01-06 10:56:00 +0100

    Updates the filter to only remove hybrid vehicles

--- src/test/java/org/entur/lamassu/leader/entityupdater/VehicleFilterTest.java
+++ src/test/java/org/entur/lamassu/leader/entityupdater/VehicleFilterTest.java
@@ -1,51 +1,22 @@
 package org.entur.lamassu.leader.entityupdater;
 
-import java.util.HashMap;
-import org.entur.lamassu.model.entities.PricingPlan;
-import org.entur.lamassu.model.entities.VehicleType;
 import org.junit.Test;
 import org.mobilitydata.gbfs.v3_0.vehicle_status.GBFSVehicle;
 
 public class VehicleFilterTest {
 
-  private static String VEHICLE_TYPE_ID = "vehicleTypeId";
-
-  private HashMap<String, VehicleType> vehicleTypesWithPricingPlan(
-    PricingPlan optionalPricingPlan
-  ) {
-    VehicleType vehicleType = new VehicleType();
-    vehicleType.setDefaultPricingPlan(optionalPricingPlan);
-    HashMap<String, VehicleType> vehicleTypes = new HashMap<>();
-    vehicleTypes.put(VEHICLE_TYPE_ID, vehicleType);
-
-    return vehicleTypes;
-  }
-
   @Test
-  public void testVehicleWithoutPricingPlanButDefaultPricingPlanIsntSkipped() {
-    VehicleFilter filter = new VehicleFilter(
-      new HashMap<>(),
-      vehicleTypesWithPricingPlan(new PricingPlan())
-    );
-
+  public void testHybridVehicleIsSkipped() {
+    VehicleFilter filter = new VehicleFilter();
     GBFSVehicle vehicle = new GBFSVehicle();
-    vehicle.setVehicleId("VehicleWithoutPricingPlan");
-    vehicle.setVehicleTypeId("vehicleTypeId");
-
-    assert filter.test(vehicle) == true;
-  }
-
-  @Test
-  public void testVehicleWithoutPricingPlanAndWithoutDefaultPricingPlanIsSkipped() {
-    VehicleFilter filter = new VehicleFilter(
-      new HashMap<>(),
-      vehicleTypesWithPricingPlan(null)
-    );
-
-    GBFSVehicle vehicle = new GBFSVehicle();
-    vehicle.setVehicleId("VehicleWithoutPricingPlan");
-    vehicle.setVehicleTypeId("vehicleTypeId");
-
+    vehicle.setStationId("TST:Station:abc123");
     assert filter.test(vehicle) == false;
+  }
+
+  @Test
+  public void testNonHybridVehicleIsNotSkipped() {
+    VehicleFilter filter = new VehicleFilter();
+    GBFSVehicle vehicle = new GBFSVehicle();
+    assert filter.test(vehicle) == true;
   }
 }
