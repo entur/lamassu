b1bebed977cdf821e4299b06d5df2efb52ad5d7f
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-01-24 13:34:44 +0100

    Use different collection mapping strategy for merge mapper

--- src/main/java/org/entur/lamassu/mapper/entitymapper/StationMergeMapper.java
+++ src/main/java/org/entur/lamassu/mapper/entitymapper/StationMergeMapper.java
@@ -1,17 +1,19 @@
 package org.entur.lamassu.mapper.entitymapper;
 
 import org.entur.lamassu.model.entities.Station;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
   componentModel = "spring",
-  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL
+  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL,
+  collectionMappingStrategy = CollectionMappingStrategy.TARGET_IMMUTABLE
 )
 public interface StationMergeMapper {
   /**
-   * Updates an existing Station with non-null values from the source Station.
+   * Updates an existing Station with values from the source Station.
    */
   void updateStation(@MappingTarget Station target, Station source);
 }
--- src/main/java/org/entur/lamassu/mapper/entitymapper/VehicleMergeMapper.java
+++ src/main/java/org/entur/lamassu/mapper/entitymapper/VehicleMergeMapper.java
@@ -1,17 +1,19 @@
 package org.entur.lamassu.mapper.entitymapper;
 
 import org.entur.lamassu.model.entities.Vehicle;
+import org.mapstruct.CollectionMappingStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
   componentModel = "spring",
-  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL
+  nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL,
+  collectionMappingStrategy = CollectionMappingStrategy.TARGET_IMMUTABLE
 )
 public interface VehicleMergeMapper {
   /**
-   * Updates an existing Vehicle with non-null values from the source Vehicle.
+   * Updates an existing Vehicle with values from the source Vehicle.
    */
   void updateVehicle(@MappingTarget Vehicle target, Vehicle source);
 }
