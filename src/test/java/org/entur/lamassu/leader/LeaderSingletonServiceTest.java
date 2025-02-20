4948bc5e3bd572d0078f22b0158d46055e30116e
Holger Bruch <hb@mfdz.de>
2025-02-20 23:43:50 +0100

    feat: add metric for outdated feeds

--- src/test/java/org/entur/lamassu/leader/LeaderSingletonServiceTest.java
+++ src/test/java/org/entur/lamassu/leader/LeaderSingletonServiceTest.java
@@ -2,6 +2,7 @@ package org.entur.lamassu.leader;
 
 import static org.mockito.Mockito.*;
 
+import org.entur.lamassu.metrics.MetricUpdater;
 import org.entur.lamassu.service.GeoSearchService;
 import org.junit.Test;
 
@@ -9,10 +10,15 @@ public class LeaderSingletonServiceTest {
 
   FeedUpdater mockedFeedUpdater = mock(FeedUpdater.class);
   GeoSearchService mockedGeoSearchService = mock(GeoSearchService.class);
+  MetricUpdater mockedMetricUpdater = mock(MetricUpdater.class);
 
   @Test
   public void testStartsScheduling() {
-    var service = new LeaderSingletonService(mockedFeedUpdater, mockedGeoSearchService);
+    var service = new LeaderSingletonService(
+      mockedFeedUpdater,
+      mockedGeoSearchService,
+      mockedMetricUpdater
+    );
     service.init();
     verify(mockedFeedUpdater).start();
   }
