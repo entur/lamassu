f1e911f99ae26d0064058034e3daa93d0b254261
Tom Erik Støwer <tom.erik.stower@entur.org>
2025-10-16 22:05:20 +0200

    fix: admin ui was not served correctly from app

--- admin-ui/vite.config.ts
+++ admin-ui/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: '../src/main/resources/static/admin',
+    outDir: '../src/main/resources/static/admin/ui',
     emptyOutDir: true,
   },
 });
